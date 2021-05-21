566291f1148d13aed2b7d32714c4c545768891af
David Kloeren <d.kloeren@googlemail.com>
2021-05-21 08:49:56 +0200

    Add Destroy Action, Fixed list#show

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -19,9 +19,10 @@ class BookmarksController < ApplicationController
   end
 
   def destroy
+    # raise
     @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
-
+    redirect_to  list_path(@bookmark.list)
   end
 
   private
