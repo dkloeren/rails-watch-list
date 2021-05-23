8dd5d84c0a3f68091a86c8dd9e6582a211064890
David Kloeren <d.kloeren@googlemail.com>
2021-05-23 17:54:57 +0200

    Added assets

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,11 +1,16 @@
 class ListsController < ApplicationController
-  before_action :set_list, only: [:show]
+  before_action :set_list, only: [:show, :destroy]
+  before_action :init_html_options, only: [:index, :show]
 
   def index
     @lists = List.all
+    @html_options[:banner] = { image: 'myke-simon-atsUqIm3wxo-unsplash.jpg' }
   end
 
   def show
+
+    # @html_options[:banner] = { image: @list.bookmarks.sample.movie.poster_url }
+    @html_options[:banner] = {}
   end
 
   def new
@@ -19,7 +24,11 @@ class ListsController < ApplicationController
     else
       render :new
     end
+  end
 
+  def destroy
+    @list.destroy
+    redirect_to lists_path
   end
 
   private
@@ -30,5 +39,9 @@ class ListsController < ApplicationController
 
   def list_params
     params.require(:list).permit(:name)
+  end
+
+  def init_html_options
+    @html_options = { action: "#{params[:controller]}-#{params[:action]}" }
   end
 end
